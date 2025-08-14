053c10cf59669a7316c334cf5db8dc2e2fbfc1a0
ParkLouis <wnghqkr30520@naver.com>
2025-08-15 03:44:31 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
+
 window.SUPABASE_URL = "%%NEXT_PUBLIC_SUPABASE_URL%%";
 window.SUPABASE_ANON = "%%NEXT_PUBLIC_SUPABASE_ANON%%";
 
