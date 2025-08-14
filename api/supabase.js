5b5d13cc240ca5577f09c0e26bc396ada1a0403c
ParkLouis <wnghqkr30520@naver.com>
2025-08-15 03:57:33 +0900

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -8,7 +8,7 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
   try {
-    const { data, error } = await supabase.from('test').select('*');
+    const { data, error } = await supabase.from('key').select('*');
     if (error) {
       console.error("Supabase error:", error);
       return res.status(500).json({ error: error.message });
